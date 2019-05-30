9525b0abafb745256d7b7ec546a02b3199110499
Tommy Tran <tranttommy95@gmail.com>
2019-05-30 10:33:15 -0700

    connected store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import App from './components/App.js';
-// import Store from './Store.js';
+import Store from './Store.js';
 
 render(
-  // <Provider store={Store}>
-  <App />,
-  // </Provider>,
+  <Provider store={Store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
